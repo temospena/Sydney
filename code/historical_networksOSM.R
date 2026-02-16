7c04c7812d99f055dab0f10605781e12e9706ae9
RFlx <temospena@gmail.com>
2026-02-16 04:38:08 +0000

    adjust with corrected sydney and add paris

--- code/historical_networksOSM.R
+++ code/historical_networksOSM.R
@@ -9,16 +9,16 @@ sydney_ci_omsactive_20260101 = st_read("data/sydney/sydney_ci_osmactive_260101.g
 sydney_ci_omsactive_20210101 = st_read("data/sydney/sydney_ci_osmactive_210101.gpkg", quiet = TRUE)
 sydney_ci_omsactive_20160101 = st_read("data/sydney/sydney_ci_osmactive_160101.gpkg", quiet = TRUE)
 
-sydney_ci_omsactive_20260101 == sydney_ci_omsactive_20160101
-
-mapview(sydney_ci_omsactive_20260101) + mapview(sydney_ci_omsactive_20160101, col.regions = "red")
-
-# they are not different!
-
-
 
 # lisbon ------------------------------------------------------------------
 
 lisbon_ci_omsactive_20260101 = st_read("data/lisbon/lisbon_ci_osmactive_260101.gpkg", quiet = TRUE)
 lisbon_ci_omsactive_20210101 = st_read("data/lisbon/lisbon_ci_osmactive_210101.gpkg", quiet = TRUE)
 lisbon_ci_osmactive_20160101 = st_read("data/lisbon/lisbon_ci_osmactive_160101.gpkg", quiet = TRUE)
+
+
+# paris ------------------------------------------------------------------
+
+paris_ci_omsactive_20260101 = st_read("data/paris/paris_ci_osmactive_260101.gpkg", quiet = TRUE)
+paris_ci_omsactive_20210101 = st_read("data/paris/paris_ci_osmactive_210101.gpkg", quiet = TRUE)
+paris_ci_osmactive_20160101 = st_read("data/paris/paris_ci_osmactive_160101.gpkg", quiet = TRUE)
