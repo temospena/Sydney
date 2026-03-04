bc47a2536bd6506cebc61c7484954e9a13ecba56
RFlx <temospena@gmail.com>
2026-03-04 07:54:49 +0000

    inspect ci evolution

--- code/pipeline/test-code/plot_ci_evolution_graphic.R
+++ code/pipeline/test-code/plot_ci_evolution_graphic.R
@@ -23,6 +23,9 @@ plot_data <- final_city_estimations %>%
   # and convert meters to kilometers
   summarise(total_ci_km = max(total_ci_m, na.rm = TRUE) / 1000, .groups = "drop")
 
+plot_data_wide = plot_data |> 
+  pivot_wider(id_cols= city, names_from = year, values_from=total_ci_km)
+
 # 2. Create the plot
 ggplot(plot_data, aes(x = year, y = total_ci_km, color = city, group = city)) +
   geom_line(linewidth = 1) +      # Draw the trend lines
