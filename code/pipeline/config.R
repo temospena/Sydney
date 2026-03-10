a671229f3b391d8faf96cbd5988ca480c7f6d3eb
RFlx <temospena@gmail.com>
2026-03-10 03:35:33 +0000

    estimate the alternative lts percentages

--- code/pipeline/config.R
+++ code/pipeline/config.R
@@ -7,18 +7,18 @@
 # Cities to process. Comment out the full list and uncomment a single name
 # to test / re-run just one city.
 target_cities <- c(
-  "Amsterdam", "Austin", # "Barcelona"
-  "Beijing", "Berlin", "Bogota",
-  "Bologna", "Brussels", "Buenos Aires",
-  "Chicago", "Christchurch", "Curitiba", "Dublin", "Gent", "Glasgow",
-  "Graz", "Hamburg", "Helsinki", "Kyoto", "Leeds", "Ljubljana",
-  "London", "Lyon", "Madrid", "Melbourne", # "Lisbon",
-  "Mexico City", "Milan", "Minneapolis", "Montpellier", "Montréal",
-  "Munich", "Nantes", "Oslo", "Portland", # "Paris", "New York",
-  "San Francisco", "Santiago", "Sao Paulo", "Seattle", "Seoul",
-  "Seville", "Shanghai", "Stockholm", "Strasbourg", "Taipei", # "Sydney",
-  "Tokyo", "Turin", "Vancouver", "Vienna", "Warsaw", "Zurich",
-  # "Sydney", "Paris", "New York", "Barcelona", "Lisbon" ## Here they are
+"Amsterdam", "Austin", # "Barcelona"
+"Beijing", "Berlin", "Bogota",
+"Bologna", "Brussels", "Buenos Aires",
+"Chicago", "Christchurch", "Curitiba", "Dublin", "Gent", "Glasgow",
+"Graz", "Hamburg", "Helsinki", "Kyoto", "Leeds", "Ljubljana",
+"London", "Lyon", "Madrid", "Melbourne", # "Lisbon",
+"Mexico City", "Milan", "Minneapolis", "Montpellier", "Montréal",
+"Munich", "Nantes", "Oslo", "Portland", # "Paris", "New York",
+"San Francisco", "Santiago", "Sao Paulo", "Seattle", "Seoul",
+"Seville", "Shanghai", "Stockholm", "Strasbourg", "Taipei", # "Sydney",
+"Tokyo", "Turin", "Vancouver", "Vienna", "Warsaw", "Zurich",
+"Sydney", "Paris", "New York", "Barcelona", "Lisbon" ## Here they are
 ) # cairo, cape town,  hong kong (> 100km in 2026)
 # target_cities <- "Lisbon"
 
